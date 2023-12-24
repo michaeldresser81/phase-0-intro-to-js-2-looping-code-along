6ddb8a25c438a5651d261bbb236c513237dbdccb
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2023-12-24 14:38:27 -0500

    complted assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const cardList = [];
+    for (let i = 0; i < names.length; i++) {
+        cardList.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardList;
+}
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i -= 1;
+    }
+}
+
